7dac58291c0b1ee5e965b433494c7173a683187d
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-21 10:28:41 +0700

    fix: update init

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,7 +1,16 @@
+import classNames from 'classnames';
 import { ReactElement } from 'react';
+
+import SvgInsert from '@/components/SvgInsert';
 
 import s from './styles.module.scss';
 
 export const Header = (): ReactElement => {
-  return <header className={s.header}>header</header>;
+  return (
+    <header className={s.header}>
+      <div className={classNames('container', s.header__container)}>
+        <SvgInsert src="/icons/logo.svg" width={60} height={60} />
+      </div>
+    </header>
+  );
 };
