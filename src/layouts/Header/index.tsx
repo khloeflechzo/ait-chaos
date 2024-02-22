c57ab2783024ee27be577ed6498007c0593e4def
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-23 00:47:47 +0700

    fix: layout mobile

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -7,6 +7,7 @@ import { ReactElement, useState } from 'react';
 import Button from '@/components/Button';
 import ImagePreload from '@/components/ImagePreload';
 import { headerButtonsData } from '@/constants/datas/header';
+import useWindowResize from '@/hooks/useWindowResize';
 
 import BuyModal from './BuyModal';
 import HeroMobile from './HeroMobile';
@@ -15,22 +16,35 @@ import s from './styles.module.scss';
 export const Header = (): ReactElement => {
   const [open, setOpen] = useState(false);
   const [openBuy, setOpenBuy] = useState(false);
+  const { isDesktop } = useWindowResize();
+
+  const handleButtonChaos = (): void => {
+    if (!isDesktop) {
+      setOpenBuy(true);
+      return;
+    } else {
+      window.location.reload();
+    }
+  };
+
   return (
     <header className={s.header}>
       <div className={classNames('container', s.header__container)}>
         <ImagePreload alt="logo" src="/icons/logo.svg" width={60} height={60} />
         <div className={s.buttons}>
-          <div
-            className={classNames(s.buttons_link, open ? s.open : '')}
-            onClick={() => setOpenBuy((prev) => !prev)}
-          >
-            {headerButtonsData.map((btn) => (
+          <div className={classNames(s.buttons_link, open ? s.open : '')}>
+            {headerButtonsData.map((btn, idx) => (
               <Button
                 key={btn.label}
                 label={btn.label}
                 child={btn.child}
                 bgColor={btn.bgColor}
                 textColor={btn.textColor}
+                onClick={() => {
+                  if (idx === headerButtonsData.length - 1) {
+                    handleButtonChaos();
+                  }
+                }}
               />
             ))}
           </div>
