aeac7ca53f5d54e414044de529bcd9bb957a83ee
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-26 17:13:04 +0700

    feat: add carousel

--- src/components/Address/index.tsx
+++ src/components/Address/index.tsx
@@ -1,17 +1,28 @@
+'use client';
+
 import cn from 'classnames';
 import Image from 'next/image';
 import React, { ReactElement } from 'react';
+
+import { truncateEthAddress } from '@/utils/functions';
 
 import ImagePreload from '../ImagePreload';
 import s from './styles.module.scss';
 
 const Address = ({ className }: { className?: string }): ReactElement => {
+  const address = '0x22a3dcb11e75c7ffbed7431c95d29a50f5936667';
+
   return (
-    <div className={cn(s.address, className)}>
+    <div
+      className={cn(s.address, className)}
+      onClick={() => navigator.clipboard.writeText(address)}
+    >
       <ImagePreload src="/icons/buttons/gray.png" alt="text-block-wrapper" priority fill />
       <button className={s.address_btn}>
         <div className={s.address_btn__content}>
-          <p>CA $CHAOS: 0x...73js</p>
+          <p>
+            CA $CHAOS: <span>{truncateEthAddress(address)}</span>
+          </p>
           <Image src="/icons/copy.svg" alt="copy" width={24} height={24} />
         </div>
       </button>
--- src/modules/Landing/Center/WrapperBox/index.tsx
+++ src/modules/Landing/Center/WrapperBox/index.tsx
@@ -5,18 +5,26 @@ import Image from 'next/image';
 import React, { PropsWithChildren, ReactElement } from 'react';
 
 import ImagePlaceHolder from '@/components/ImagePlaceHolder';
+import { useCubesStore } from '@/stores/useCubesStore';
 
 import s from './styles.module.scss';
 
 const WrapperBox = ({ children }: PropsWithChildren): ReactElement => {
+  const { toggleActiveCard, activeCard } = useCubesStore();
+
   return (
     <div className={s.wrapper}>
       <ImagePlaceHolder src="/images/nft-box.png" fill alt="ntf-box" className={s.image} />
-      {/* <div className={s.elipse}></div> */}
-      <button className={classNames(s.btn, s.btn_prev)}>
+      <button
+        className={classNames(s.btn, s.btn_prev)}
+        onClick={() => toggleActiveCard(activeCard === 0 ? 4 : activeCard - 1)}
+      >
         <Image alt="prev-btn" src="/images/prev-btn.png" fill />
       </button>
-      <button className={classNames(s.btn, s.btn_next)}>
+      <button
+        className={classNames(s.btn, s.btn_next)}
+        onClick={() => toggleActiveCard(activeCard === 4 ? 0 : activeCard + 1)}
+      >
         <Image alt="next-btn" src="/images/next-btn.png" fill />
       </button>
       {children}
