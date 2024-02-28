2f45c2d7b8aadc7ee63b69339b282742e3c6e1c1
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-28 16:28:38 +0700

    fix: remove unuse

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -25,12 +25,9 @@ const Button = ({
   const buttonClasses = cn(s.button, s[`button__${bgColor}`], classess);
   return (
     <div onClick={onClick} className={buttonClasses}>
-      <SvgInsert src={`/icons/buttons/corner/green.svg`} className={cn(s.corner, s.corner__left)} />
+      <SvgInsert src={`/icons/buttons/corner.svg`} className={cn(s.corner, s.corner__left)} />
       <div className={s.button__slice}></div>
-      <SvgInsert
-        src={`/icons/buttons/corner/green.svg`}
-        className={cn(s.corner, s.corner__right)}
-      />
+      <SvgInsert src={`/icons/buttons/corner.svg`} className={cn(s.corner, s.corner__right)} />
       <button className={s.button_content}>
         <TypoLabel color={textColor} className={s.label}>
           {label}
