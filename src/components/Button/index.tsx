9be934cdce4fc8ae075b077951b8093a7d5367ca
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-28 11:40:27 +0700

    feat: button pixel

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,8 @@ import React, { ReactElement } from 'react';
 
 import { ButtonProps } from '@/types/common';
 
-import ImagePreload from '../ImagePreload';
+// import ImagePreload from '../ImagePreload';
+import SvgInsert from '../SvgInsert';
 import { TypoLabel } from '../Typo';
 import s from './styles.module.scss';
 
@@ -24,13 +25,15 @@ const Button = ({
   const buttonClasses = cn(s.button, s[`button__${bgColor}`], classess);
   return (
     <div onClick={onClick} className={buttonClasses}>
-      <ImagePreload src={`/icons/buttons/${bgColor}.png`} alt="text-block-wrapper" priority fill />
+      {/* <ImagePreload src={`/icons/buttons/${bgColor}.png`} alt="text-block-wrapper" priority fill /> */}
+      <SvgInsert src={`/icons/buttons/corner/${bgColor}.svg`} className={cn(s.corner)} />
       <button className={s.button_content}>
         <TypoLabel color={textColor} className={s.label}>
           {label}
         </TypoLabel>
         {!isNil(child) && child}
       </button>
+      <SvgInsert src={`/icons/buttons/corner/${bgColor}.svg`} className={cn(s.corner)} />
     </div>
   );
 };
