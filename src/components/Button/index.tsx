15ec2247f0ed232f50c2d13db337e26a38f2b8c5
HHHDTrinh <trinhhhd82@gmail.com>
2024-02-28 16:17:43 +0700

    fix: pixel button desktop style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -25,15 +25,18 @@ const Button = ({
   const buttonClasses = cn(s.button, s[`button__${bgColor}`], classess);
   return (
     <div onClick={onClick} className={buttonClasses}>
-      {/* <ImagePreload src={`/icons/buttons/${bgColor}.png`} alt="text-block-wrapper" priority fill /> */}
-      <SvgInsert src={`/icons/buttons/corner/${bgColor}.svg`} className={cn(s.corner)} />
+      <SvgInsert src={`/icons/buttons/corner/green.svg`} className={cn(s.corner, s.corner__left)} />
+      <div className={s.button__slice}></div>
+      <SvgInsert
+        src={`/icons/buttons/corner/green.svg`}
+        className={cn(s.corner, s.corner__right)}
+      />
       <button className={s.button_content}>
         <TypoLabel color={textColor} className={s.label}>
           {label}
         </TypoLabel>
         {!isNil(child) && child}
       </button>
-      <SvgInsert src={`/icons/buttons/corner/${bgColor}.svg`} className={cn(s.corner)} />
     </div>
   );
 };
